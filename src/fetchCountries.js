0d8b8b1dff63d45f0ac45c53eb529301690aa821
Pavel Lobtsov <plobtsov@gmail.com>
2022-04-11 23:23:59 +0300

    update hw

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URl = 'https://restcountries.com/v3.1/name/';
 const params = 'name,capital,population,flags,languages';
 
 export const fetchCountries = name => {
-  fetch(`${BASE_URl}${name}?fields=${params}`).then(response => {
+  return fetch(`${BASE_URl}${name}?fields=${params}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
