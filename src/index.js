c21c7cdb3e13bc0a40f873e43c59e17a01ab2f5a
Pavel Lobtsov <plobtsov@gmail.com>
2022-04-12 17:28:06 +0300

    update hw10

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import listTemplate from './templates/list.hbs';
+import cardTemplate from './templates/card.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
+const listEL = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
 
 const onInputName = event => {
   const searchName = event.target.value.trim();
   if (searchName.length > 0) {
     fetchCountries(searchName)
       .then(data => {
-        console.log(data);
+        innerData(data);
       })
       .catch(error => {
-        console.log('Такої країни не існує!');
+        Notify.failure('Oops, there is no country with that name');
         return;
       });
   } else {
-    console.log('Введите страну');
+    Notify.failure('Please enter country');
+    listEL.innerHTML = '';
+    infoEl.innerHTML = '';
+  }
+};
+
+const innerData = data => {
+  if (data.length === 1) {
+    listEL.innerHTML = '';
+    infoEl.innerHTML = cardTemplate(data);
+  } else {
+    infoEl.innerHTML = '';
+    listEL.innerHTML = listTemplate(data);
+  }
+  if (data.length > 10) {
+    listEL.innerHTML = '';
+    Notify.info('Too many matches found. Please enter a more specific name.');
   }
 };
 
